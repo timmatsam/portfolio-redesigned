0927e8aab6c7df0fe775d7edfa442c1cd8ebb08b
timmatsam <timmatsam@gmail.com>
2023-03-30 11:38:40 -0400

    build: change tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{jsx,js,tsx,ts}"],
+  content: ["./src/**/*.{jsx,js,tsx,ts}", "./public/index.html"],
   theme: {
     extend: {},
   },
