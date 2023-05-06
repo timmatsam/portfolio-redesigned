823e68ba6248cc8f34c74b3cab326cea6196756c
timmatsam <timmatsam@gmail.com>
2023-05-06 09:04:24 -0500

    feat: add active path color change

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -7,13 +7,13 @@ const HeroSection = () => {
             <p>A minimalist typesafe website, </p>
             <div className="flex flex row">
                 <p className="mr-1">with designs inspired by</p>
-                <p className="font-bold">Midjourney</p>
+                <p className="underline">Midjourney</p>
             </div>
             <div className='flex flex-row'>
                 <p className="mr-1">Developed with </p>
-                <p className='font-bold mr-1'> Next.js </p>
+                <p className='underline mr-1'> Next.js </p>
                 <p className="mr-1">and Deployed through </p>
-                <p className='font-bold'>Vercel</p>
+                <p className='underline'>Vercel</p>
             </div>
 
 
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,19 +1,32 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 export const NavBar = () => {
-    return (<nav className="flex flex-wrap items-center justify-center p-6 col-span-2">
-        <ul className="flex justify-end items-center list-none">
-            <li className="mr-3">
-                <Link className="hover:text-gray-400 hover:font-semibold" href="/">Home</Link>
-            </li>
-            <li className="mr-3">
-                <Link className="hover:text-gray-400 hover:font-semibold" href="/about">About</Link>
-            </li>
-            <li className="mr-3">
-                <Link className="hover:text-gray-400 hover:font-semibold" href="/projects">Projects</Link>
-            </li>
-            {/* <li className="mr-3">
-                <Link className="hover:text-gray-400 hover:font-semibold" href="/thesis">Thesis</Link>
-            </li> */}
-        </ul>
-    </nav>);
-}
+  const router = useRouter();
+
+  return (
+    <nav className="flex flex-wrap items-center justify-center p-6 col-span-2">
+      <ul className="flex justify-end items-center list-none">
+        <li className="mr-3">
+          <Link className={`hover:text-gray-400 hover:font-semibold ${router.pathname === '/' ? 'text-cyan-700 font-semibold' : ''}`} href="/">Home</Link>
+        </li>
+        <li className="mr-3">
+          <Link className={`hover:text-gray-400 hover:font-semibold ${router.pathname === '/about' ? 'text-cyan-700 font-semibold' : ''}`} href="/about">About</Link>
+        </li>
+        <li className="mr-3">
+          <Link className={`hover:text-gray-400 hover:font-semibold ${router.pathname === '/projects' ? 'text-cyan-700 font-semibold' : ''}`} href="/projects">Projects</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+
+
+
+
+
+
+{/* <li className="mr-3">
+    <Link className="hover:text-gray-400 hover:font-semibold" href="/thesis">Thesis</Link>
+</li> */}
