ff1e4dd169123af147c1e1c3ee24c8dc207c8ca6
timmatsam <timmatsam@gmail.com>
2023-05-06 08:42:25 -0500

    feat: update sections w more info

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
 
 const HeroSection = () => {
     return (
         <div id='hero-section' className='flex flex-col items-center '>
             <h1>Tim Samuel</h1>
             <h3>Software Developer</h3>
-            <p>A minimalist website designed by Midjourney</p>
+            <p>A minimalist typesafe website, </p>
+            <div className="flex flex row">
+                <p className="mr-1">with designs inspired by</p>
+                <p className="font-bold">Midjourney</p>
+            </div>
+            <div className='flex flex-row'>
+                <p className="mr-1">Developed with </p>
+                <p className='font-bold mr-1'> Next.js </p>
+                <p className="mr-1">and Deployed through </p>
+                <p className='font-bold'>Vercel</p>
+            </div>
+
+
+            
         </div>
     )
 }
--- pages/about.tsx
+++ pages/about.tsx
@@ -7,7 +7,16 @@ const About = () => {
         <Head>
         <title>About</title>
       </Head>
-            <p className='w-1/2'>I have a kindle. My spotify playlist is fairly diverse in terms of language but mainly Japanese (I love jpop). I can't fathom a world without Chopin. I enjoy traveling - domestically and internationally. I NEVER pay for a checked bag. Here's a tip: always bring a duffel bag and backpack instead of paying $50 - $100 each way for luggage. That's theft. My current passions involve watching/reading thing after thing in regards to urban planning and design. America has poor infrastructure; our public transporation and our inability to provide high/mid-density housing instead of detached, single family homes is not prudent. Trust me, you'll be much happier if you could walk/bike to your local grocery store to pick up that A5 miyazaki wagyu. Trains are cool. A Super Conducting Maglev train that travels at 600km/hr is WICKED COOL. Also, I think I'm kind of funny. Alright, I'm done.</p>
+            <p className='w-1/2'>I have a kindle. My spotify playlist is a combination of French, 
+            Japanese, Koream, Hindi, and yeah English. I can't fathom a world without Chopin. 
+            I enjoy traveling - AND I NEVER pay for a checked bag. 
+            Here's a tip: replace that backpack with a duffel bag (or carry-on if your ticket includes it) instead of paying each way 
+            for luggage. That's theft. My current passions involve watching/reading thing after thing in 
+            regards to urban planning and design. America has poor infrastructure; our public transit 
+            and our inability to provide high/mid-density housing instead of detached, single family homes is 
+            not prudent. Trust me, you'll be much happier if you could walk/bike to your local grocery store to 
+            pick up that A5 miyazaki wagyu. Trains are cool. A Super Conducting Maglev train that travels at 600km/hr is 
+            WICKED COOL. Also, I think I'm kind of funny. Alright, I'm done.</p>
             <Image src={headshot} alt="headshot" className=' w-5/12 shrink-0 pl-10'/>
         </div>
      
