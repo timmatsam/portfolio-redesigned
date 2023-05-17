e3310ab58eb3fdd317f2fd2a2b85e1bd6c04d221
timmatsam <timmatsam@gmail.com>
2023-05-16 20:37:34 -0400

    refactor: add typing for pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
 import Head from "next/head";
-import  HeroSection  from "../components/HeroSection";
+import HeroSection from "@/components/HeroSection";
 import mbpro from "../public/mb-pro.png";
 import Image from "next/image";
+import Layout from "@/components/Layout";
+import { ReactElement } from "react";
 const Home = () => (
-        <div className='grid grid-rows-1fr-7fr grid-cols-[40%,60%] justify-items-center items-center'>
-        <Head>
-          <title>Home</title>
-        </Head>
-            <HeroSection />
-            <Image src={mbpro} alt="mb-pro-with-rain" />
-        </div>
-       
+  <div className="grid grid-rows-1fr-7fr grid-cols-[40%,60%] justify-items-center items-center">
+    <Head>
+      <title>Home</title>
+    </Head>
+    <HeroSection />
+    <Image src={mbpro} alt="mb-pro-with-rain" />
+  </div>
 );
+Home.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
 export default Home;
+//TODO: update the preview link to match the landing page of the website, also update the title that shows (from Tim Samuel to Portfolio)
+//TODO: add a new favicon icon with my face on it
