d20a7c97cf60e2a86ff9488033485c0d1b2655d4
timmatsam <timmatsam@gmail.com>
2023-05-28 20:50:49 -0400

    perf: use innate next fonts

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,24 @@
 import { Footer } from "./Footer";
 import { ReactNode } from "react";
 import { Navbar } from "./Navibar";
+import { Barlow_Condensed } from "next/font/google";
+
+const barlow = Barlow_Condensed({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-barlow",
+});
 interface LayoutProps {
   children?: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="App">
-      <div className="Box">
+    <div
+      className={`bg-gradient-to-b from-linear1 to-linear2 lg:from-main lg:to-main w-screen h-screen flex justify-center items-center flex-col ${barlow.variable} font-sans lg:space-y-20 overflow-y-auto`}
+    >
+      <div className="lg:bg-gradient-to-b from-linear1 to-linear2 w-screen h-screen lg:w-3/5 lg:h-4/5 lg:rounded-xl lg:shadow-2xl shadow-shadow justify-self-start overflow-y-auto">
         <Navbar />
         {children}
       </div>
@@ -16,5 +26,7 @@ const Layout = ({ children }: LayoutProps) => {
     </div>
   );
 };
-
+/**
+ * make
+ */
 export default Layout;
