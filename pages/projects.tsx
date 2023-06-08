7411af1c00b5851d60a9bddfbf2a3db113763944
timmatsam <timmatsam@gmail.com>
2023-06-08 13:19:38 -0400

    style: update responsiveness

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -11,37 +11,57 @@ const Projects = () => {
       <Head>
         <title>Projects</title>
       </Head>
-      <div className="flex flex-col justify-around items-start w-full lg:h-full overflow-y-auto space-y-5">
-        <div className="flex flex-col lg:flex-row">
+      <div className="flex flex-col justify-around items-center lg:items-start w-5/6 ml-6 lg:w-full lg:h-full lg:space-y-0 mx-2">
+        <div className="flex flex-col lg:grid lg:grid-cols-2">
           <Link
             href="https://github.com/2009-FSA-CS-Lucians-Lightbringers/haircutzGame"
-            className="shrink h-full  lg:h-6/12 lg:pl-10 hover:opacity-50"
+            className="hover:opacity-50"
           >
-            <Image alt="haircutz" src={haircutzImage} width={600} />
+            <Image
+              alt="haircutz"
+              src={haircutzImage}
+              width={600}
+            />
           </Link>
 
           <div className="lg:px-10">
-            <h3 className="font-semibold">Haircutz</h3>
-            <p>A 1v1 online real time strategy game.</p>
+            <h3 className="font-semibold">
+              Haircutz
+            </h3>
             <p>
-              Built using the phaser framework along with WebSocket technology.
+              A 1v1 online real time strategy
+              game.
+            </p>
+            <p>
+              Built using the phaser framework
+              along with WebSocket technology.
             </p>
           </div>
         </div>
-        <div className="flex flex-col lg:flex-row ">
+        <div className="flex flex-col lg:grid lg:grid-cols-2 ">
           <Link
             href="https://github.com/The-Shopper-Awakens/graceshopper"
-            className="shrink lg:w-8/12  lg:pl-10 hover:opacity-50"
+            className="hover:opacity-50"
           >
-            <Image alt="outdoor" src={outdoorImage} width={600} />
+            <Image
+              alt="outdoor"
+              src={outdoorImage}
+              width={600}
+            />
           </Link>
           <div className="lg:px-10">
-            <h3 className="font-semibold">Outdoor Equipment Inc. (OEI)</h3>
+            <h3 className="font-semibold">
+              Outdoor Equipment Inc. (OEI)
+            </h3>
             <p>
-              An ecommerce website, i.e. a recreation of REI, a well known
+              An ecommerce website, i.e. a
+              recreation of REI, a well known
               retailer for outdoors enthusiasts.{" "}
             </p>
-            <p>Built using a React frontend and Node.js backend.</p>
+            <p>
+              Built using a React frontend and
+              Node.js backend.
+            </p>
           </div>
         </div>
       </div>
